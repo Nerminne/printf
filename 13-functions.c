1fc82ea3303663a0676d4b14b6a9e7b69ffc8279
Nerminne <nerminee.mostafaa@gmail.com>
2023-09-19 16:39:47 -0700

    fixing errors

--- 13-functions.c
+++ 13-functions.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * reverso - function that reverse string
- * @arg: argument
- * Return: reversed string
+ * @args: argument
+ * Return: reversed string length
  */
 int reverso(va_list args)
 {
 	char *stringo = va_arg(args, char *);
 	int y = 0, x = 0;
 	char *reversedo;
-	
+
 	x = (int)strlen(stringo);
 	reversedo = malloc(sizeof(char) * x);
 	if (stringo == NULL || reversedo == NULL)

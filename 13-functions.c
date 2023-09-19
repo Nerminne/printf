cba1f040415892d4c8984bbf73edb5715a88dac9
Nerminne <nerminee.mostafaa@gmail.com>
2023-09-19 12:23:36 -0700

    reverse string function

---
+++ 13-functions.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * reverso - function that reverse string
+ * @arg: argument
+ * Return: reversed string
+ */
+int reverso(va_list args)
+{
+	char *stringo = va_arg(args, char *);
+	int y = 0, x = 0;
+	char *reversedo;
+	
+	x = (int)strlen(stringo);
+	reversedo = malloc(sizeof(char) * x);
+	if (stringo == NULL || reversedo == NULL)
+		return (-1);
+	x = (int)strlen(stringo);
+	x--;
+	while (x >= 0)
+	{
+		reversedo[y] = stringo[x];
+		x--;
+		y++;
+	}
+	write(1, reversedo, y);
+	free(reversedo);
+	return (y);
+}
